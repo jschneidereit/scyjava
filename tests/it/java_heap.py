a89064a01f199d9e6aba67e90732e6fa3d3202ce
jschneidereit <schneidereit@pm.me>
2024-07-20 16:50:41 -0700

    fix: loosen tolerance for comparing memory consumption

--- tests/it/java_heap.py
+++ tests/it/java_heap.py
@@ -6,6 +6,11 @@ from assertpy import assert_that
 
 import scyjava
 
+
+def magnitude(x: int) -> int:
+    return math.floor(math.log10(abs(x)))
+
+
 mb_initial = 50  # initial MB of memory to snarf up
 
 scyjava.config.set_heap_min(mb=mb_initial)
@@ -24,5 +29,7 @@ assert_that(mb_used, 'Used memory should be less than the current memory total')
 assert_that(mb_total, 'current memory total should be less than maximum memory').is_less_than_or_equal_to(mb_max)
 assert_that(mb_max, 'maximum heap size should be approx. 1 GB').is_between(900, 1024)
 
-assert_that(mb_used, 'most memory should be available').is_less_than(5)
-assert_that(mb_total, 'total memory should be close to initial').is_close_to(mb_initial, tolerance=5)
+tolerance = pow(10, magnitude(mb_initial))
+
+assert_that(mb_used, 'most memory should be available').is_less_than(tolerance)
+assert_that(mb_total, 'total memory should be close to initial').is_close_to(mb_initial, tolerance=tolerance)
